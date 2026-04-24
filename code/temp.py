c702bbab8ceacc5c0c0e38cee28b3440ac3463d1
a-paranjape <aseem@iucaa.in>
2026-04-24 19:25:29 +0530

    partial update

--- code/temp.py
+++ code/temp.py
@@ -1,18 +1,18 @@
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
-
+from matplotlib.collections import PatchCollection
 
 #############################################
 class IllustrateNetwork(Utilities):
     """ Illustrate architecture of an existing network or network ensemble. """
     ###################
-    def __init__(self,base_dir,file_stem,ensemble,family='seq',verbose=True,logfile=None):
+    def __init__(self,base_dir,file_stem,ensemble,family,verbose=True,logfile=None):
         """ Load existing network or network ensemble and create simple graphical illustration of its architecture.
             -- base_dir: str, path/to/folder where network (ensemble) was originally created from.
             -- file_stem: str, file stem (relative to base_dir) used for defining location of network (ensemble).
             -- ensemble: bool, whether location is an ensemble (True) or single network (False).
-            -- family: str [default 'seq']; one of 'seq' (Sequential), 'biseq' (BiSequential), 'gan' (GAN); needed if ensemble is False.
+            -- family: str; one of 'seq' (Sequential), 'biseq' (BiSequential), 'gan' (GAN)
             -- verbose,logfile: usual I/O control
             Methods:
             -- 
@@ -23,6 +23,8 @@ class IllustrateNetwork(Utilities):
         self.verbose = verbose
         self.logfile = logfile
 
+        self.golden_inv = 2/(1+np.sqrt(5))
+        
         setup_dict = {}
         setup_dict['X'] = np.zeros((1,10)) # dummy arrays
         setup_dict['Y'] = np.zeros((1,10)) # for HyperOpt
@@ -72,21 +74,77 @@ class IllustrateNetwork(Utilities):
 
 
     ###################
-    def illustrate_single(self,net,ax):
+    def illustrate_single(self,params,ax):
         """ Create illustration object of single network. 
-            -- net: object of type self.family
+            -- params: setup params dict of desired single network
             --  ax: matplotlib axis object on which to draw
         """
-        bdry = np.array([[0,0],[0,1],
-                         [1,0],[1,1]]) # define boundary
+        bdry = np.array([[0,0],[0,self.golden_inv],
+                         [1,0],[1,self.golden_inv]]) # define boundary
         bdry = bdry.T
         ax.plot(bdry[0],bdry[1],c='w') # set boundary
         ax.set_xticks([])              # kill tick marks
         ax.set_yticks([])
-
+        rect_bdry = Rectangle((0,0),1,1,lw=1,edgecolor='k',facecolor='none') # draw outer boundary
+        ax.add_patch(rect_bdry)
+        
+        horz_margin = 0.02 # frac to exclude on left/right
+        vert_margin = 0.05 # frac to exclude on top/bottom
+        xfrac = 0.75       # frac of (layer+sep) given to layer
+        Dy_max = 1 - 2*vert_margin # max height of rectangles (as frac)
+        Dy_min = 0.05 # min height of rectangles (as frac)
+        assert (Dy_min < Dy_max)
+        
+        ######################
+        # Currently only for Sequential family
+        ######################
         # draw L rectangles, one for each layer, of equal width
         # height proportional to layer width for each rectangle
+        L = params['L'] # number of layers excluding input layer
+        dx_full = (1 - 2*horz_margin)/L
+        dx = dx_full*xfrac
+        xsep = dx_full - dx
+        
+        min_hw,max_hw = np.min(params['n_layer'][:-1]),np.max(params['n_layer'][:-1]) # min,max widths of hidden layers
+        n0 = params['data_dim']
+        nlast = params['n_layer'][:-1]
+        
+        W_min = np.min([n0,min_hw]) # don't incldue nlast (in case nlast=1)
+        W_max = np.min([n0,max_hw,nlast])
+        W_ratio = W_max/(W_min + 1e-15)
+        W = np.concatenate(([n0],params['n_layer']))
+        
+        if W_ratio >= 10.0:
+            # log scaling
+            dydw = (Dy_max-Dy_min)/np.log(W_ratio)
+            Dy = Dy_max + dydw*np.log(W/W_max)
+        elif W_ratio >= 3.0:
+            # sqrt scaling
+            dydw = (Dy_max-Dy_min)/np.sqrt(W_ratio)
+            Dy = Dy_max + dydw*np.sqrt(W/W_max)
+        else:
+            # linear scaling
+            dydW = (Dy_max-Dy_min)/(W_max - W_min + 1e-15)
+            Dy = Dy_max + dydW*(W - W_max)
 
+        # IN PROGRESS HERE
+        layers = []
+        for l in range(L-1):
+            # loop over hidden layers
+            pass
+
+
+        # # Loop over data points; create box from errors at each point
+        # errorboxes = [Rectangle((x - xe[0], y - ye[0]), xe.sum(), ye.sum())
+        #               for x, y, xe, ye in zip(xdata, ydata, xerror.T, yerror.T)]
+
+        # # Create patch collection with specified colour/alpha
+        # pc = PatchCollection(errorboxes, facecolor=facecolor, alpha=alpha,
+        #                      edgecolor=edgecolor)
+
+        # # Add collection to Axes
+        # ax.add_collection(pc)
+            
         return 
     ###################
     
